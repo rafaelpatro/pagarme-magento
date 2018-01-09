943373b270173e1b18ef3b6cc6636e3081784ef5
Leonam Pereira Dias <leonam.dias@pagar.me>
2018-01-09 21:04:52 -0200

    chore: implement method to visit the refused transactions page
    Implement the step responsible to navigate, through magento menu, to
    navigate to pagar.me refused transactions page

--- tests/acceptance/RefusedTransactionsContext.php
+++ tests/acceptance/RefusedTransactionsContext.php
@@ -7,4 +7,28 @@ require_once __DIR__ . '/../../vendor/autoload.php';
 class RefusedTransactionsContext extends RawMinkContext
 {
     use \PagarMe\Magento\Test\Helper\AdminAccessProvider;
+
+    /**
+     * @When I go to Pagar.me refused transactions page
+     */
+    public function iGoToPagarMeRefusedTransactionsPage()
+    {
+        $session = $this->getSession();
+        $page = $session->getPage();
+        $this->closeAdminPopup($page);
+
+        $page->find('named', ['link', 'Pagar.me'])
+            ->mouseOver();
+
+        $page->find('named', ['link', 'Refused Transactions'])
+            ->click();
+    }
+
+    /**
+     * @Then a refused transactions list should be visible
+     */
+    public function aRefusedTransactionsListShouldBeVisible()
+    {
+        throw new PendingException();
+    }
 }
