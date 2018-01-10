af206190e3b53c846fd27767d95e259c96003f06
Leonam Pereira Dias <leonam.dias@pagar.me>
2018-01-10 18:52:52 -0200

    wip: create refused transactions controller

---
+++ app/code/community/PagarMe/RefusedTransactions/controllers/Adminhtml/RefusedTransactionsController.php
@@ -0,0 +1,14 @@
+<?php
+
+class PagarMe_RefusedTransactions_Adminhtml_RefusedTransactionsController extends Mage_Adminhtml_Controller_Action
+{
+    public function indexAction()
+    {
+        echo 'hello world';
+    }
+
+    public function refusedtransactionsAction()
+    {
+        echo 'hola mundo';
+    }
+}
